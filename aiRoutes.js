7afc7897812a486cfbafdf634b0ee7ebb85ced98
aashana-04 <aashana.july04@gmail.com>
2026-05-08 09:06:51 +0530

    Delete aiRoutes.js

--- aiRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const { getGuidance } = require("../controllers/aiController");
-
-router.post("/guidance", getGuidance);
-
-module.exports = router;
